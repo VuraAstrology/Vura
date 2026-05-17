bc026a485a4ced8c6f6747bc8e7c50f53fb24024
Maria Eduarda Moraes Vieira <mariaeduardamoraesvieira71@gmail.com>
2026-05-17 15:56:31 -0300

    redefinição de senha/ envio de email de redefinição de senha usando o nodemailer

--- banco/server.js
+++ banco/server.js
@@ -1,3 +1,8 @@
+require('dotenv').config();
+
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
+
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
@@ -13,6 +18,18 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+
+//________EMAIL___________________________________________________________________
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
 
 // ─── CADASTRO ────────────────────────────────────────────────────────────────
 app.post('/cadastro', async (req, res) => {
@@ -91,39 +108,70 @@ app.post('/login', async (req, res) => {
 
 
 
-// _____ESQUECI A SENHA DA MINHA CONTA _______________________________________________________________________
-app.post('/esqueci-senha', async (req, res) =>{
-  const {email} = req.body;
-  try {
-    const [row] = await pool.query('SELECT * FROM usuarios WHERE email = ?',[email]);
+// _____ESQUECI A SENHA DA MINHA CONTA (envia email de recuperação de senha)_______________________________________________________________________
+
+app.post('/esqueci', async(req,res) =>{
+  const { email} = req.body;
+  const emailNormalizado = email?.trim().toLowerCase();
+
+ console.log('Email recebido:',JSON.stringify(emailNormalizado));
+  try{
+    const [rows] = await pool.query('SELECT * FROM usuarios WHERE email = ?',[emailNormalizado]);
     if (rows.length === 0){
-      return res.status(404).json({erro: 'Usuário não encontrado.'});
+      return res.status(404).json({erro:'Usuário não encontrado, Email inválido!'});
     }
-    const usuario = rows[0];
+    const usuario = rows[0]; //retorna o primeiro registro do usuário encontrado
     const token = crypto.randomBytes(32).toString('hex');
-    const expira = new Date(Date.now() + 1000 * 60 * 15 );
-    await pool.query('INSERT INTO recuperacao_senha (usuario_id, token, expira_em) VALUES (?,?,?)',[
-      usuario.id,
-      token,
-      expira
-    ]);
-    const link = 'https://localhost:5500/resetar.html?token=${token}';
+    const expira = new Date(Date.now() + 1000 * 60 * 15) // 15min para expirar o token de redefinição de senha
+    await pool.query(`INSERT INTO recuperacao_senha (usuario_id, token, expira_em) VALUES (?,?,?)`,[usuario.id,token,expira]);
+
+   const link = `http://localhost:5500/Vura/resetar.html?token=${token}`;
     await transporter.sendMail({
       to: email,
-      subject: 'Recuperação de senha Vura',
-      html:'<h1>Recupere sua senha de acesso Vura</h1><br><a href="${link}" >Redefinir senha</a>'
+      subject: 'Recuperação de senha de acesso Vura',
+      html:`<h2>Recuperação de senha</h2>
+      <p> recupere sua senha de acesso a sua conta VURA clicando no link abaixo:</p>
+      <a href="${link}">Redefinir senha</a>`
     });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({erro: 'Ocorreu um erro interno.'});
+    return res.status(200).json({mensagem:'Email de redefinição de senha enviado com sucesso!'});
+  } catch (err){
+    console.error('ERRO INTERNO:', err);
+    return res.status(500).json({erro:'Não foi ímpossivel conectar com o servidor no momento, tente novamente mais tarde!'});
   }
-
 });
 
-//___________________RESETAR MINHA SENHA (nova senha/atualização)___________________________________________
 
+//________________________resetar senha de acesso__________________________________________________________
 
+app.post('/resetar', async (req, res) =>{
+  const { token, novaSenha } = req.body;
 
+  try{
+    if (novaSenha.length < 6){
+      return res.status(400).json({erro:'Senha muito pequena'});
+    }
+    const [rows] = await pool.query(`
+      SELECT * FROM recuperacao_senha
+      WHERE token = ?
+      AND expira_em > NOW()`,[token]);
+
+      if(rows.length === 0){
+        return res.status(400).json({erro:'Token inválido ou expirado.'});
+      }
+    const recuperacao = rows[0];
+    const hash = await bcrypt.hash(novaSenha,10);
+
+    await pool.query(`
+      UPDATE usuarios SET senha = ?
+      WHERE id = ?`,[hash, recuperacao.usuario_id]);
+
+      await pool.query(`DELETE FROM recuperacao_senha WHERE token = ?`, [token]);
+      return res.status(200).json({mensagem:'Senha atualizada com sucesso!'});
+  }catch(err){
+    console.error('Não foi possível alterar a senha',err);
+    return res.status(500).json({erro:'Não foi possível alterar a senha no momento tente novamente mais tarde!'});
+  }
+});
 
 
 // ─── START ────────────────────────────────────────────────────────────────────
